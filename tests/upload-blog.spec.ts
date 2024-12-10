47c3ffbbfafc5d4847edb6e03a8c72cc4e08fdd6
WillHDev <will.v.holcombe@gmail.com>
2024-12-09 18:05:05 -0600

    upload blog test is passing, but publish feature is turned off until delete endpoint is accessible

---
+++ tests/upload-blog.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('Upload blog', async ({ page }) => {
+  await page.goto('https://thewildernet.com/home');
+
+  await page.getByText('Upload a Blog').click();
+  await page.setInputFiles('input[type="file"]', "C:\\Users\\willv\\Documents\\wildernet\\tests\\images\\marginalRevolution.jpg");
+  await page.getByRole('button', { name: "Crop & Apply"}).click();
+  await page.getByPlaceholder('Enter Blog URL').fill('https://marginalrevolution.com');
+  await page.getByPlaceholder('Enter Blog title').fill('Marginal Revolution');
+  await page.getByPlaceholder('Add a description of the blog').fill('An economics blog about culture and assorted fascinations from around the internet');
+  await page.getByRole('button', { name: "Continue" }).click();
+ // await page.getByRole('button', { name: "Publish" }).click();
+});
